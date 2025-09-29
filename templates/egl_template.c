883c3b8f5064ad659f84f08967ff5e239951ef8e
Ben Gray <ben.gray@sky.uk>
2025-09-29 09:50:33 +0100

    Remove generation of GL / EGL extension functions.
    Remove creating stub for these functions as don't want clients
    to assume all the extensions are present.  Instead want them
    to discover them using the standard EGL / GL functions.

--- templates/egl_template.c
+++ templates/egl_template.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
-#define EGL_EGLEXT_PROTOTYPES 1
+// Don't define EGL_EGLEXT_PROTOTYPES, we don't want to give the false impression the platform implements any of these
+// EGL extensions, instead require clients to use the function pointer approach to get any extensions they want.
+// #define EGL_EGLEXT_PROTOTYPES
 
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
--- templates/gles2_template.c
+++ templates/gles2_template.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
-#define GL_GLEXT_PROTOTYPES 1
+// Don't define GL_GLEXT_PROTOTYPES, we don't want to give the false impression the platform implements any of these
+// GLES2/3 extensions, instead require clients to use the function pointer approach to get any extensions they want.
+// #define GL_GLEXT_PROTOTYPES 1
 
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
