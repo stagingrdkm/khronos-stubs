572f795496f6e8263b01608206e6dd620d9b0781
Ben Gray <68104043+bgray-sky@users.noreply.github.com>
2025-12-01 11:27:56 +0000

    CMFSUPPORT-3319: Add bunch of requested licensing changes. (#4)
    Added license blurbs and references to other licenses. Also
    added a CONTRIBUTING.md file.

--- stubs/gles2_stubs.c
+++ stubs/gles2_stubs.c
@@ -1,4 +1,23 @@
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2025 Sky UK
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This file contains generated function stubs from Khronos OpenGL ES 2 & 3 headers.
 
 // Don't define GL_GLEXT_PROTOTYPES, we don't want to give the false impression the platform implements any of these
 // GLES2/3 extensions, instead require clients to use the function pointer approach to get any extensions they want.
